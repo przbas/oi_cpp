c12548b70fcb382250226010ce5559268da3051f
przbas <przemyslaw.baskiewicz@gmail.com>
2025-01-31 14:34:34 +0100

    początek

---
+++ poc.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    cout << "Mount Everest - 8850 m n.p.m.\n";
+    cout << "McKinley - 6194 m n.p.m.\n";
+    cout << "Mont Blanc - 4810 m n.p.m.";
+
+    return 0;
+}
