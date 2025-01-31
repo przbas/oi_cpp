519708cce0052a272626c45b443feb3efa55ee9e
przbas <przemyslaw.baskiewicz@gmail.com>
2025-01-31 12:29:38 +0100

    nowy plik nao

---
+++ nao.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //cin.tie(0)->sync_with_stdio(0);
+    int a, b, c;
+
+    cout << "Podaj trzy liczby całkowite rozdzielone spacjami\n";
+    cin >> a >> b >> c;
+
+    cout << a << " " << b << " " << c << "\n";
+    cout << c << " " << b << " " << a << "\n";
+
+
+    return 0;
+}
