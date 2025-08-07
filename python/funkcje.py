e31e962dcbd156604b4ebb4a83ec7d713fb8d809
przbas <przemyslaw.baskiewicz@gmail.com>
2025-08-07 10:03:50 +0200

    testowanie funkcji

--- python/funkcje.py
+++ python/funkcje.py
@@ -1,12 +1,47 @@
 # obramuj
-
 def obramuj(a):
     print(f"*** {a} ***")
 
-print("Wpisz dwie cyfry:")
-x = input("pierwsza: ")
-y = input("druga: ")
 
-obramuj(x)
-obramuj(y)
-obramuj(x+y)
+# print("Wpisz dwie cyfry:")
+# x = input("pierwsza: ")
+# y = input("druga: ")
+
+# obramuj(x)
+# obramuj(y)
+# obramuj(x+y)
+
+
+# szescian
+def szescian(krawedz):
+    return krawedz**3
+
+# x = float(input("Podaj długość krawędzi sześcianu: "))
+# print(szescian(x))
+
+# dwa parametry
+def min(a, b):
+    if a < b:
+        return a
+    else:
+        return b
+
+# w = input()
+# x = int(w.split()[0])
+# y = int(w.split()[1])
+# z = int(w.split()[2])
+# print(min(x + y, y * z))
+
+# zliczanie cyfr
+def liczba_cyfr(n):
+    if n == 0:
+        return 1
+    wyn = 0
+    while n > 0:
+        wyn += 1
+        n //= 10  # dzielenie całkowite
+    return wyn
+
+# n = int(input("Podaj liczbę całkowitą: "))
+# print(f"Liczba cyfr w liczbie {n} wynosi: {liczba_cyfr(n)}")
+
